a3a8fd8b5c713f042e2198e6fc0156a4c555d37e
ddksaku <ddksaku@gmail.com>
2015-04-06 11:37:40 -0700

    updated

--- app/src/androidTest/java/com/demo/pdf/ApplicationTest.java
+++ app/src/androidTest/java/com/demo/pdf/ApplicationTest.java
@@ -1,6 +1,8 @@
 package com.demo.pdf;
 
 import android.test.ActivityInstrumentationTestCase2;
+
+import com.artifex.mupdfdemo.MuPDFActivity;
 import com.robotium.solo.Solo;
 
 
@@ -11,11 +13,15 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 @SuppressWarnings("rawtypes")
-public class ApplicationTest extends ActivityInstrumentationTestCase2 {
+public class ApplicationTest extends ActivityInstrumentationTestCase2<MuPDFActivity> {
+
 
         private Solo solo;
         private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.demo.pdf.MainActivity";
+        private static final String TEST_FILE_NAME = "test.pdf";
+
         private static Class<?> launcherActivityClass;
+
 
         static{
             try {
@@ -26,13 +32,15 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2 {
         }
         @SuppressWarnings("unchecked")
         public ApplicationTest() throws ClassNotFoundException {
-            super(launcherActivityClass);
+            super(MuPDFActivity.class);
         }
 
         public void setUp() throws Exception {
+
             super.setUp();
-            solo = new Solo(getInstrumentation());
-            getActivity();
+            solo=newSolo(getInstrumentation(),getActivity());
+
+
         }
         @Override
         public void tearDown() throws Exception {
@@ -40,14 +48,58 @@ public class ApplicationTest extends ActivityInstrumentationTestCase2 {
             super.tearDown();
         }
         public void testRun() {
+            copyAssets();
 
-            solo.waitForActivity("MainActivity", 2000);
-            solo.clickOnButton("SHOW PDF CONTENT");
-            solo.scrollDown();
-            solo.scrollToBottom();
-            solo.scrollUp();
+            String path = "/storage/emulated/0/Android/data/com.mupdfdemo/files/test.pdf"
+            core = openFile(path);
+
             solo.takeScreenshot();
         }
+        private void copyAssets() {
+            AssetManager assetManager = getAssets();
+            String[] files = null;
+            try {
+                files = assetManager.list("");
+            } catch (IOException e) {
+                Log.e("tag", "Failed to get asset file list.", e);
+            }
 
+            for(String filename : files) {
+                if (filename.compareTo(TEST_FILE_NAME) == 0) {
+                    InputStream in = null;
+                    OutputStream out = null;
+                    try {
+                        in = assetManager.open(filename);
+                        outFile = new File(getExternalFilesDir(getApplicationContext()), filename);
+                        out = new FileOutputStream(outFile);
+                        copyFile(in, out);
+                    } catch (IOException e) {
+                        Log.e("tag", "Failed to copy asset file: " + filename, e);
+                    } finally {
+                        if (in != null) {
+                            try {
+                                in.close();
+                            } catch (IOException e) {
+                                // NOOP
+                            }
+                        }
+                        if (out != null) {
+                            try {
+                                out.close();
+                            } catch (IOException e) {
+                                // NOOP
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private void copyFile(InputStream in, OutputStream out) throws IOException {
+            byte[] buffer = new byte[1024];
+            int read;
+            while((read = in.read(buffer)) != -1){
+                out.write(buffer, 0, read);
+            }
+        }
 
 }
--- app/src/main/java/com/demo/pdf/MainActivity.java
+++ app/src/main/java/com/demo/pdf/MainActivity.java
@@ -1,6 +1,8 @@
 package com.demo.pdf;
 
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,48 +17,81 @@ import android.widget.Button;
 import com.artifex.mupdfdemo.MuPDFActivity;
 import com.artifex.mupdfdemo.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 
 public class MainActivity extends Activity {
 
-
+    private static final String TEST_FILE_NAME = "test.pdf";
     private static final int PICKFILE_RESULT_CODE = 10;
-
+    Intent intent;
+    File outFile;
+    Uri fileUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        Button showPDFBtn = (Button)findViewById(R.id.btn_show_pdf);
-        showPDFBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-              openFileBrowser();
+//        setContentView(R.layout.activity_main);
+
+          intent = new Intent(this, MuPDFActivity.class);
+          copyAssets();
+
+         intent.setAction(Intent.ACTION_VIEW);
+
+         fileUri = Uri.parse(outFile.getAbsolutePath());
+         intent.setData(fileUri);
+
+        intent.putExtra("password", "PDF document password");
+
+                //if you need highlight link boxes
+        intent.putExtra("linkhighlight", true);
+
+        //if you don't need device sleep on reading document
+        intent.putExtra("idleenabled", false);
+
+
+        //document name
+        intent.putExtra("docname", "PDF document file name");
+
+        startActivity(intent);
+
+//            }
+//        });
+
+
+}
+    public static File getFilesDirectory(Context context) {
+        File appFilesDir = null;
+        if (Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
+            appFilesDir = getExternalFilesDir(context);
+        }
+        if (appFilesDir == null) {
+            appFilesDir = context.getFilesDir();
+        }
+        return appFilesDir;
+    }
+    private static File getExternalFilesDir(Context context) {
+        File dataDir = new File(new File(Environment.getExternalStorageDirectory(), "Android"), "data");
+        File appFilesDir = new File(new File(dataDir, context.getPackageName()), "files");
+        if (!appFilesDir.exists()) {
+            if (!appFilesDir.mkdirs()) {
+                //L.w("Unable to create external cache directory");
+                return null;
             }
-        });
+            try {
+                new File(appFilesDir, ".nomedia").createNewFile();
+            } catch (IOException e) {
+                //L.i("Can't create \".nomedia\" file in application external cache directory");
+                return null;
+            }
+        }
+        return appFilesDir;
     }
 
-    private void openFileBrowser() {
-        Intent fileintent = new Intent(Intent.ACTION_GET_CONTENT);
-        fileintent.setType("application/pdf");
-        try {
-            startActivityForResult(fileintent, PICKFILE_RESULT_CODE);
-        } catch (ActivityNotFoundException e) {
-            Log.e("tag", "No activity can handle picking a file. Showing alternatives.");
-        }
-    }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Fix no activity available
-        if (data == null)
-            return;
-        switch (requestCode) {
-            case PICKFILE_RESULT_CODE:
-                if (resultCode == RESULT_OK) {
-                    String FilePath = data.getData().getPath();
-                    //FilePath is your file as a string
-                    showPdfContent(FilePath);
-                }
-        }
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,21 +100,52 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    private void showPdfContent(String filePath) {
-
-        Uri uri = Uri.parse(filePath);
-
-
-        Intent intent = new Intent(this, MuPDFActivity.class);
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setData(uri);
-
-
+    private void copyAssets() {
+        AssetManager assetManager = getAssets();
+        String[] files = null;
         try {
-            startActivity(intent);
-        } catch (ActivityNotFoundException e) {
-            e.printStackTrace();
+            files = assetManager.list("");
+        } catch (IOException e) {
+            Log.e("tag", "Failed to get asset file list.", e);
+        }
+
+        for(String filename : files) {
+            if (filename.compareTo(TEST_FILE_NAME) == 0) {
+                InputStream in = null;
+                OutputStream out = null;
+                try {
+                    in = assetManager.open(filename);
+                    outFile = new File(getExternalFilesDir(getApplicationContext()), filename);
+                    out = new FileOutputStream(outFile);
+                    copyFile(in, out);
+                } catch (IOException e) {
+                    Log.e("tag", "Failed to copy asset file: " + filename, e);
+                } finally {
+                    if (in != null) {
+                        try {
+                            in.close();
+                        } catch (IOException e) {
+                            // NOOP
+                        }
+                    }
+                    if (out != null) {
+                        try {
+                            out.close();
+                        } catch (IOException e) {
+                            // NOOP
+                        }
+                    }
+                }
+            }
         }
     }
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
+    }
+
 }
 
